b3f528a4223dc541a96a9dbe01d85970741b1097
tianjiangyu <161840237@nuaa.edu.cn>
2023-05-24 11:44:39 +0800

    第一阶段：MobileNetV2模型

--- models/MobileNet.py
+++ models/MobileNet.py
@@ -1,11 +1,149 @@
 import torch
 import torch.nn as nn
 from torch import Tensor
-from typing import Type, Any, Callable, Union, List, Optional
-from collections import abc, OrderedDict
+from typing import Any, Callable,List, Optional
+from collections import abc
 
-""" conv + norm + activation """
+class MobileNetV2(nn.Module):
+    def __init__(
+            self,
+            input_channels: int = 3,
+            num_classes: int = 1000,
+            width_mult: float = 1.0,
+            round_nearest: int = 8,
+            dropout: float = 0.2,
+    ) -> None:
+        """
+            input_channels: 输入图像的通道数，默认通道数为3
+            num_classes: MobileNetV2的输出维度，默认为1000
+        """
+        super(MobileNetV2, self).__init__()
+        block = InvertedResidual
+        norm_layer = nn.BatchNorm2d
+
+        input_channels_modify = 32
+        last_channel = 1280
+
+        inverted_residual_setting = [
+            # t, c, n, s
+            # expand ratio,out channel,range epoch,stride
+            [1, 16, 1, 1],
+            [1, 24, 2, 2],
+            [0.5, 64, 4, 2],
+            [0.5, 96, 3, 1],
+            [6, 160, 1, 2],
+            [6, 160, 2, 2],
+            [6, 320, 1, 1],
+        ]
+
+        # 构建模型第一层
+        input_channels_modify = _make_divisible(input_channels_modify * width_mult, round_nearest)
+        self.last_channel = _make_divisible(last_channel * max(1.0, width_mult), round_nearest)
+
+        features: List[nn.Module] = [
+            ConvNormActivation(input_channels, input_channels_modify, stride=2, norm_layer=norm_layer, activation_layer=nn.ReLU6)
+        ]
+
+        # 构建 inverted residual blocks
+        for t, c, n, s in inverted_residual_setting:
+            output_channel = _make_divisible(c * width_mult, round_nearest)
+            for i in range(n):
+                stride = s if i == 0 else 1
+                features.append(block(input_channels_modify, output_channel, stride, expand_ratio=t, norm_layer=norm_layer))
+                input_channels_modify = output_channel
+
+        # 构建模型最后一层
+        features.append(
+            ConvNormActivation(input_channels_modify, self.last_channel, kernel_size=1, norm_layer=norm_layer,activation_layer=nn.ReLU6)
+        )
+
+
+        self.features = nn.Sequential(*features)
+        self.classifier = nn.Sequential(
+            nn.AdaptiveAvgPool2d((1, 1)),
+            nn.Flatten(),
+            nn.Dropout(p=dropout),
+            nn.Linear(self.last_channel, num_classes),
+        )
+        self.len1 = len(self.features)
+        self.len2 = len(self.classifier)
+
+        # weight initialization
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode="fan_out")
+                if m.bias is not None:
+                    nn.init.zeros_(m.bias)
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.ones_(m.weight)
+                nn.init.zeros_(m.bias)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.zeros_(m.bias)
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = self.features(x)
+        x = self.classifier(x)
+        return x
+
+    def __len__(self):
+        return len(self.features) + len(self.classifier)
+
+    def __iter__(self):
+        """ 用于遍历MobileNetV2模型的每一层 """
+        return SentenceIterator(self.features,self.classifier)
+
+    def __getitem__(self, item):
+        try:
+            if item < self.len1:
+                layer = self.features[item]
+            else:
+                layer = self.classifier[item - self.len1]
+        except IndexError:
+            raise StopIteration()
+        return layer
+
+
+class SentenceIterator(abc.Iterator):
+    """
+        MobileNetV2迭代器
+        下面是 MobileNetV2 网络的迭代参数调整
+        将下面的设置传入到 MobileNetV2 的 __iter__ 中可以完成对于 MobileNetV2 网络的层级遍历
+    """
+    def __init__(self,features, classifier):
+        self.features = features
+        self.classifier = classifier
+
+        self._index = 0
+
+        self.len1 = len(features)
+        self.len2 = len(classifier)
+
+    def __next__(self):
+        try:
+            if self._index < self.len1:
+                layer = self.features[self._index]
+            else:
+                len = self.len1
+                layer = self.classifier[self._index - len]
+
+        except IndexError:
+            raise StopIteration()
+        else:
+            self._index += 1
+        return layer
+
+
+def mobilenet_v2(**kwargs: Any) -> MobileNetV2:
+    model = MobileNetV2(**kwargs)
+    return model
+
+
 class ConvNormActivation(torch.nn.Sequential):
+    """
+        MobileNet-v2 ConvNormActivation-DNN块设计
+        由三个部分组成：Conv + Norm + Activation
+    """
     def __init__(
         self,
         in_channels: int,
@@ -24,36 +162,38 @@ class ConvNormActivation(torch.nn.Sequential):
             padding = (kernel_size - 1) // 2 * dilation
         if bias is None:
             bias = norm_layer is None
-        layers = [torch.nn.Conv2d(in_channels,out_channels,kernel_size,stride,padding,dilation=dilation,groups=groups,bias=bias)]
+        layers = [
+            torch.nn.Conv2d(in_channels=in_channels,
+                            out_channels=out_channels,
+                            kernel_size=(kernel_size,kernel_size),
+                            stride=(stride,stride),
+                            padding=padding,
+                            dilation=(dilation,dilation),
+                            groups=groups,
+                            bias=bias)
+        ]
         if norm_layer is not None:
-            layers.append(norm_layer(out_channels))
+            layers.append(
+                norm_layer(out_channels)
+            )
         if activation_layer is not None:
             params = {} if inplace is None else {"inplace": inplace}
-            layers.append(activation_layer(**params))
+            layers.append(
+                activation_layer(**params)
+            )
         super(ConvNormActivation, self).__init__(*layers)
         self.out_channels = out_channels
 
 
-
-
-class InvertedResidual_SentenceIterator(abc.Iterator):
-    def __init__(self,conv):
-        self.conv = conv
-        self._index = 0
-        self.len = len(conv)
-
-    def __next__(self):
-        try:
-            layer = self.conv[self._index]
-        except IndexError:
-            raise StopIteration()
-        else:
-            self._index += 1
-        return layer
-
-
-""" conv：pw + dw + pw """
 class InvertedResidual(nn.Module):
+    """
+        MobileNet-v2 ConvNormActivation-InvertedResidualBlock设计
+        由下面几个部分组成：
+        1) ConvNormActivation block
+        2) ConvNormActivation block
+        3) Conv2d layer
+        4) Norm layer
+    """
     def __init__(self, inp: int, oup: int, stride: int, expand_ratio,
                  norm_layer: Optional[Callable[..., nn.Module]] = None) -> None:
         super(InvertedResidual, self).__init__()
@@ -75,7 +215,7 @@ class InvertedResidual(nn.Module):
                 # dw
                 ConvNormActivation(hidden_dim, hidden_dim, stride=stride, groups=hidden_dim, norm_layer=norm_layer,activation_layer=nn.ReLU6),
                 # pw-linear
-                nn.Conv2d(hidden_dim, oup, kernel_size=1, stride=1, padding=0, bias=False),
+                nn.Conv2d(hidden_dim, oup, kernel_size=(1,1), stride=(1,1), padding=0, bias=False),
                 norm_layer(oup),
             ])
 
@@ -106,6 +246,28 @@ class InvertedResidual(nn.Module):
 
 
 
+class InvertedResidual_SentenceIterator(abc.Iterator):
+    """
+        InvertedResidual迭代器
+        下面是 InvertedResidual 网络的迭代参数调整
+        将下面的设置传入到 InvertedResidual 的 __iter__ 中可以完成对于 InvertedResidual 网络的层级遍历
+    """
+    def __init__(self,conv):
+        self.conv = conv
+        self._index = 0
+        self.len = len(conv)
+
+    def __next__(self):
+        try:
+            layer = self.conv[self._index]
+        except IndexError:
+            raise StopIteration()
+        else:
+            self._index += 1
+        return layer
+
+
+
 def _make_divisible(v: float, divisor: int, min_value: Optional[int] = None) -> int:
     if min_value is None:
         min_value = divisor
@@ -114,130 +276,3 @@ def _make_divisible(v: float, divisor: int, min_value: Optional[int] = None) -> 
     if new_v < 0.9 * v:
         new_v += divisor
     return new_v
-
-
-class SentenceIterator(abc.Iterator):
-    def __init__(self,features, classifier):
-        self.features = features
-        self.classifier = classifier
-
-        self._index = 0
-
-        self.len1 = len(features)
-        self.len2 = len(classifier)
-
-    def __next__(self):
-        try:
-            if self._index < self.len1:
-                layer = self.features[self._index]
-            else:
-                len = self.len1
-                layer = self.classifier[self._index - len]
-
-        except IndexError:
-            raise StopIteration()
-        else:
-            self._index += 1
-        return layer
-
-
-class MobileNetV2(nn.Module):
-    def __init__(
-            self,
-            num_classes: int = 1000,
-            width_mult: float = 1.0,
-            round_nearest: int = 8,
-            dropout: float = 0.2,
-    ) -> None:
-        super(MobileNetV2, self).__init__()
-        block = InvertedResidual
-        norm_layer = nn.BatchNorm2d
-
-        input_channel = 32
-        last_channel = 1280
-
-        inverted_residual_setting = [
-            # t, c, n, s
-            # expand ratio,out channel,range epoch,stride
-            [1, 16, 1, 1],
-            [1, 24, 2, 2],
-            [0.5, 64, 4, 2],
-            [6, 96, 3, 1],
-            [6, 160, 1, 2],
-            [6, 160, 2, 2],
-            [6, 320, 1, 1],
-        ]
-
-        # building first layer
-        input_channel = _make_divisible(input_channel * width_mult, round_nearest)
-        self.last_channel = _make_divisible(last_channel * max(1.0, width_mult), round_nearest)
-
-        features: List[nn.Module] = [
-            ConvNormActivation(3, input_channel, stride=2, norm_layer=norm_layer, activation_layer=nn.ReLU6)
-        ]
-
-        # building inverted residual blocks
-        for t, c, n, s in inverted_residual_setting:
-            output_channel = _make_divisible(c * width_mult, round_nearest)
-            for i in range(n):
-                stride = s if i == 0 else 1
-                features.append(block(input_channel, output_channel, stride, expand_ratio=t, norm_layer=norm_layer))
-                input_channel = output_channel
-
-        # building last several layers
-        features.append(
-            ConvNormActivation(input_channel, self.last_channel, kernel_size=1, norm_layer=norm_layer,activation_layer=nn.ReLU6)
-        )
-
-        # make it nn.Sequential
-        self.features = nn.Sequential(*features)
-
-        # building classifier
-        self.classifier = nn.Sequential(
-            nn.AdaptiveAvgPool2d((1, 1)),
-            nn.Flatten(),
-            nn.Dropout(p=dropout),
-            nn.Linear(self.last_channel, num_classes),
-        )
-
-        self.len1 = len(self.features)
-        self.len2 = len(self.classifier)
-
-        # weight initialization
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode="fan_out")
-                if m.bias is not None:
-                    nn.init.zeros_(m.bias)
-            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
-                nn.init.ones_(m.weight)
-                nn.init.zeros_(m.bias)
-            elif isinstance(m, nn.Linear):
-                nn.init.normal_(m.weight, 0, 0.01)
-                nn.init.zeros_(m.bias)
-
-    def forward(self, x: Tensor) -> Tensor:
-        x = self.features(x)
-        x = self.classifier(x)
-        return x
-
-    def __len__(self):
-        return len(self.features) + len(self.classifier)
-
-    def __iter__(self):
-        return SentenceIterator(self.features,self.classifier)
-
-    def __getitem__(self, item):
-        try:
-            if item < self.len1:
-                layer = self.features[item]
-            else:
-                layer = self.classifier[item - self.len1]
-        except IndexError:
-            raise StopIteration()
-        return layer
-
-
-def mobilenet_v2(**kwargs: Any) -> MobileNetV2:
-    model = MobileNetV2(**kwargs)
-    return model
